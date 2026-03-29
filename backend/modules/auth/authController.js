089ddeac5327082397aecceac1fe11cb2dd535f6
Sundaram-Katare <sundaramkatare53@gmail.com>
2026-03-29 10:51:19 +0530

    login endpoint

--- backend/modules/auth/authController.js
+++ backend/modules/auth/authController.js
@@ -6,13 +6,13 @@ export const registerUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
-        if(!email || !password) {
+        if (!email || !password) {
             return res.status(400).json({ message: "Email and Password are required" });
         }
 
         const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
 
-        if(existingUser.rows.length > 0) {
+        if (existingUser.rows.length > 0) {
             return res.status(400).json({ message: "User with this email already exists" });
         }
 
@@ -27,3 +27,33 @@ export const registerUser = async (req, res) => {
         return res.status(500).json({ message: "Error Registering User", err });
     }
 };
+
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: "Email and Password are required" });
+        }
+
+        const existingUser = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+
+        if (existingUser.rows.length == 0) {
+            return res.status(400).json({ message: "No User with this email" });
+        }
+
+        const user = existingUser.rows[0];
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid Password" });
+        }
+
+        const token = generateToken(user);
+
+        return res.status(200).json({ message: "Login Successful", user, token });
+    } catch (err) {
+        return res.status(500).json({ message: "Error in Login", err });
+    }
+}
