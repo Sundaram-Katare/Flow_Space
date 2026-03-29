9253fcb983bc5573474dfdff72810690bf8af4b2
Sundaram-Katare <sundaramkatare53@gmail.com>
2026-03-29 17:09:40 +0530

    hero Section UI improvement

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,15 @@
 import Navbar from "../components/Navbar";
-import { easeInOut, motion, propEffect } from 'framer-motion';
+import { delay, eachAxis, easeInOut, motion, propEffect, scale } from 'framer-motion';
+import { ArrowRight, ArrowRightIcon } from 'lucide-react';
+
+const sentence = "Manage tasks, chat with your team, and track progress all in one place.";
+
+const charVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: { opacity: 1, y: 0 },
+  delay: 0.6
+};
+
 
 export default function Home() {
     return (
@@ -8,13 +18,80 @@ export default function Home() {
                 <Navbar />
 
                 <div className="flex flex-col items-center justify-center h-full py-32 space-y-12">
-                    <div className="text-center text-black text-7xl font-semibold font-poppins">
+                    {/* <div className="text-center text-black text-7xl font-semibold font-poppins">
                         Collaborate and Organize work effortlessly
+                    </div> */}
+
+                    <div className="space-y-10">
+                        <div className="grid grid-cols-2 gap-2">
+                            <div className="flex">
+                                <motion.h1 className="font-poppins text-6xl font-semibold"
+                                    initial={{ opacity: 0, x: -20 }}
+                                    animate={{ opacity: 1, x: -5 }}
+                                    transition={{ duration: 0.8, ease: easeInOut }}
+                                >
+                                    All Your
+                                </motion.h1>
+
+                                <motion.h1 className="font-poppins text-6xl font-semibold"
+                                    initial={{ opacity: 0, x: -5 }}
+                                    animate={{ opacity: 1, x: -0 }}
+                                    transition={{ duration: 0.8, ease: easeInOut, delay: 0.6, stiffness: 200 }}>
+                                    Work
+                                </motion.h1>
+                            </div>
+
+                            <div className="flex">
+                                    <motion.h1 className="font-poppins text-6xl font-semibold"
+                                        initial={{ opacity: 0, x: 5 }}
+                                        animate={{ opacity: 1, x: 0 }}
+                                        transition={{ duration: 0.8, ease: easeInOut, delay: 0.6 }}
+                                    >
+                                        Finally
+                                    </motion.h1>
+
+                                    <motion.h1 className="font-poppins text-6xl font-semibold"
+                                        initial={{ opacity: 0, x: 20 }}
+                                        animate={{ opacity: 1, x: 5 }}
+                                        transition={{ duration: 0.8, ease: easeInOut }}>
+                                        in Sync
+                                    </motion.h1>
+                                </div>
+                        </div>
+
+                        <motion.div className="flex-0 flex justify-center" delay="2.6" >
+                            <div className="flex items-center justify-center">
+                                <div>
+                                    <motion.button 
+                                      className="flex px-2 py-2 text-2xl rounded-md text-white bg-blue-800"
+                                       initial={{ opacity: 0, scale: 0.1 }}
+                                             animate={{ opacity: 1, scale: 1 }}
+                                             transition={{ duration: 0.6, delay: 0.8 }}
+                                    >
+                                        Create Your Workspace
+
+                                        <div className="flex items-center justify-center rounded bg-white text-black px-1 ml-1">
+                                          <ArrowRightIcon />
+                                        </div>
+                                    </motion.button>
+                                </div>
+                            </div>
+                        </motion.div>
                     </div>
 
-                    <p className="text-xl text-black text-center">
-                        Manage tasks, chat with your team, and track progress all in one place.
-                    </p>
+                    <motion.p
+        className="text-xl text-black text-center"
+        initial="hidden"
+        animate="visible"
+        transition={{ staggerChildren: 0.05, delay: 0.6 }} // controls speed
+      >
+        {sentence.split("").map((char, index) => (
+          <motion.span key={index} variants={charVariants}>
+            {char}
+          </motion.span>
+        ))}
+      </motion.p>
+
 
                     <div className="relative py-12">
                         <div className=" grid grid-cols-2 gap-16 px-4">
