117aae40b586c08d860ea19ec11432a776c15e7a
Sundaram-Katare <sundaramkatare53@gmail.com>
2026-04-14 22:32:08 +0530

    User Queries

--- backend/Tables/users.js
+++ backend/Tables/users.js
@@ -5,11 +5,15 @@ export const createUserTable = async () => {
     await pool.query(`
       CREATE TABLE IF NOT EXISTS users (
         id SERIAL PRIMARY KEY,
-        name VARCHAR(100),
         email VARCHAR(100) UNIQUE NOT NULL,
+        username VARCHAR(100) UNIQUE NOT NULL,
         password TEXT NOT NULL,
+        first_name VARCHAR(100),
+        last_name VARCHAR(100),
         avatar TEXT,
-        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        status VARCHAR(20) DEFAULT 'online',
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
       );
     `);
     console.log("Users Table Created Successfully");
@@ -19,15 +23,41 @@ export const createUserTable = async () => {
   }
 };
 
-export const updateTable = async () => {
+export const getUserById = async (userId) => {
   try {
-    await pool.query(
-      `ALTER TABLE users
-       ADD COLUMN status VARCHAR(20)
-      `
-    )
+     const result = await pool.query(
+      'SELECT id, email, username, first_name, last_name, avatar FROM users WHERE id = $1',
+      [userId]
+     );
   } catch (err) {
-    console.log("Error Updating Table", err);
+    console.log('Error Getting User', err);
     throw err;
   }
-}
+};
+
+export const getUserByEmail = async (email) => {
+  try {
+    const result = await pool.query(
+      'SELECT * FROM users WHERE email = $1', 
+      [email]
+    );
+  } catch (err) {
+    console.log("Error Getting User: ", err);
+    throw err;
+  }
+};
+
+export const createUser = async (email, username, hashedPassword, firstName, lastName) => {
+  try {
+    const result = await pool.query(
+      `
+      INSERT INTO users (email, username, password, first_name, last_name)
+      VALUES ($1, $2, $3, $4) 
+      RETURNING id, email, username, first_name, last_name
+      `, [email, username, hashedPassword, firstName, lastName]
+    );
+  } catch (err) {
+    console.log("Error Creating User: ", err);
+    throw err;
+  }
+};
