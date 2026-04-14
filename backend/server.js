47ef75e94270cb233a3772de560637d10251aa6d
Sundaram-Katare <sundaramkatare53@gmail.com>
2026-04-14 22:21:26 +0530

    fixed middleware issue

--- backend/server.js
+++ backend/server.js
@@ -18,15 +18,13 @@ const io = new Server(server, {
 app.use(express.json());
 app.use(cors());
 
-
-app.use((req, res) => {
-    logger.info(`${req.method} ${req.path}`);
-    next();
-});
-
 //Health Check
 app.get("/api/health", (req, res) => {
     res.json({ status: "OK", message: "Server is running" });
+});
+
+app.use((req, res) => {
+    logger.info(`${req.method} ${req.path}`);
 });
 
 io.on('connection', (socket) => {
